eb1c0723c4e95b61794a52f00664bcb5289aa306
mariya-dzivinska <mariya.dzivinska@gmail.com>
2015-10-17 12:54:11 +0300

    add project

---
+++ FileTemplateManager/Controllers/ManageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileTemplateManager.Controllers
+{
+	public class ManageController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public string Save()
+		{
+			return "Hello world!";
+		}
+	}
+}
---
+++ FileTemplateManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileTemplateManager.MvcApplication" Language="C#" %>
