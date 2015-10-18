2f8f556c39a4ca98f137363887065ceeb92d3f96
mariya-dzivinska <mariya.dzivinska@gmail.com>
2015-10-18 23:40:34 +0300

    commit missed changes

--- FileTemplateManager/Controllers/HomeController.cs
+++ FileTemplateManager/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileTemplateManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,12 @@ namespace FileTemplateManager.Controllers
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		public ActionResult SetAnswer()
+		{
+			var model = new AnswerModel();
+			return View(model);
 		}
 	}
 }
